2c1e6175032c783109fc42bf47a0d1a5e184a53d
Sascha Mehlhase <sascha.mehlhase@cern.ch>
2023-07-29 20:49:48 +0200

    Update update_repo.sh to try something

--- .gitlab/update_repo.sh
+++ .gitlab/update_repo.sh
@@ -1,17 +1,17 @@
 #!/bin/bash
-# exit when any command fails
-set -e
-# echo on
-set -x
-git checkout master
-git remote add upstream https://github.com/clelange/cds_paper_bot.git
-git fetch upstream
-if [[ -n $(git log ..upstream/master) ]]; then
-    git config --global user.email "${GITMAIL}"
-    git config --global user.name "${GITNAME}"
-    git merge upstream/master -m "merge with upstream"
-    git remote set-url origin "${REMOTE_GIT_REPO}"
-    git push origin HEAD
-else
-    echo "No changes found."
-fi
+# # exit when any command fails
+# set -e
+# # echo on
+# set -x
+# git checkout master
+# git remote add upstream https://github.com/clelange/cds_paper_bot.git
+# git fetch upstream
+# if [[ -n $(git log ..upstream/master) ]]; then
+#     git config --global user.email "${GITMAIL}"
+#     git config --global user.name "${GITNAME}"
+#     git merge upstream/master -m "merge with upstream"
+#     git remote set-url origin "${REMOTE_GIT_REPO}"
+#     git push origin HEAD
+# else
+#     echo "No changes found."
+# fi
